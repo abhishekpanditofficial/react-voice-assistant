abeee3a7a9f27839f4d18232d4abb2440ddff326
abhishekpanditofficial <abhishekpanditoff@gmail.com>
2021-09-06 16:19:08 +0530

    complete react-news-voice-assistant

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
+import { Typography } from '@material-ui/core';
+
+const alanKey= '79826ce58f467a8f29ef99acaa8cea612e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
+  const classes= useStyles();
+  const [newsArticles,setNewsArticle]= useState([]);
+ const [activeArticles,setActiveArticles]= useState(-1);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand:({command, articles,number}) =>{
+        if(command === 'newHeadlines'){
+          
+         setNewsArticle(articles);
+         setActiveArticles(-1);
+        } else if(command === 'highlight'){
+            setActiveArticles((p) => p + 1);   
+        } else if(command === 'open'){
+           const parsedNumber= number.length > 2 ? wordsToNumbers(number,{fuzzy: true}) : number;
+           const article= articles[parsedNumber - 1];
+          
+         
+          if(parsedNumber > 20){
+            alanBtn().playText('Please try that again');
+          }else {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening.....');
+          }
+          
+        }
+      }
+
+    })
+  }, [])
+
+  //https://tender-mahavira-ce4b52.netlify.app/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+      <div className={classes.logoContainer}> 
+      <img src="https://media-exp1.licdn.com/dms/image/C560BAQGN6aSFSg3mXQ/company-logo_200_200/0/1614676658593?e=2159024400&v=beta&t=RYkOBRgczMgnc7OPJ_yYgVmKZ3Z_x3X-TkvyJBI8hpg" className={classes.alanLogo} alt="alan logo"/>
+      {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+ 
+      </div>
+      <NewsCards articles={newsArticles} activeArticles={activeArticles} />
+     </div>
   );
 }
 
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,42 @@
+import React,{useState,useEffect,createRef} from 'react'
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import useStyles from './styles';
+import classNames from 'classnames';
+
+function NewsCard({article: {description,publishedAt,source,title,url,urlToImage},i,activeArticles}) {
+  const classes= useStyles();
+  const [elRefs,setElRefs]= useState([]);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+  useEffect(() =>{
+      setElRefs((refs) => Array(20).fill().map((_,i) => refs[i] || createRef()));
+  }, [])
+
+  useEffect(() =>{
+     if(i === activeArticles && elRefs[activeArticles]){
+         scrollToRef(elRefs[activeArticles]);
+     }
+  }, [i,activeArticles,elRefs])
+
+
+    return (
+       <Card ref={elRefs[i]} className={classNames(classes.card,activeArticles === i ? classes.activeCard : null)}>
+           <CardActionArea href={url} target="_blank">
+               <CardMedia className={classes.media} image={urlToImage || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnwVoZ1Gm4M0iyL6nPI2goFi5olJ2jdJSRvg&usqp=CAU'} />
+               <div className={classes.details}>
+                   <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                   <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+               </div>
+               <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
+               <CardContent>
+                   <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+               </CardContent>
+           </CardActionArea>
+           <CardActions className={classes.cardActions}>
+               <Button size="small" color="primary">Learn More</Button>
+               <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+           </CardActions>
+       </Card>
+    )
+}
+
+export default NewsCard
